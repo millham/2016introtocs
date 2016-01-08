1f6e3c71f535b57f6a821b3bf91bf82199af2bfc
Lili Hammond <lili181@stmaryspdx.net>
2016-01-08 10:29:18 -0800

    i eat beans

---
+++ javascript/beeb beeb.js
@@ -0,0 +1,23 @@
+// javascript stuff goes here
+console.log("goob")
+// <script type="text/javascript">
+	// function link() {
+        // var link_s = document.getElementById('link_id').value;
+        // document.getElementById('link_str').innerHTML = link_s.link()
+    // }
+    // object.onclick=function(){myScript};
+    // var name = document.getElementsByclassName("name");
+    // var tp = document.getElementsByclassName("tooltip");
+
+    // function show(){
+        // tp.style.display="block";
+//    }
+//    function hide(){
+//        tp.style.display="";
+//    }
+var myimgobj = document.images["jsbutton2"];
+function changeImage()
+{
+	document.images["jsbutton"].src="http://vignette3.wikia.nocookie.net/fallout/images/4/40/FO3_Pork_n'_Beans.png/revision/latest?cb=20130531211348";
+	return true;
+}
